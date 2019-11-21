e5cd65d5c7c48fa3123a4a0fc2ce986b0164a89e
forrest <forrest@iolo.com>
2019-11-21 11:02:23 -0800

    sitemap-xml dynamic frequency, blog modified date as most recent

--- content/plugins/sitemap-xml/sitemap-xml.php
+++ content/plugins/sitemap-xml/sitemap-xml.php
@@ -6,12 +6,20 @@ Author: FC
 Version: 0.1
 Author URI: http://f4rrest.wordpress.com
 License: GPL2
-*/ 
+*/
 add_action( 'publish_post', 'itsg_create_sitemap' );
 add_action( 'publish_page', 'itsg_create_sitemap' );
 
 function itsg_create_sitemap() {
 
+    // get date of latest post
+    $args = array(
+      'post_type' => array('post','page'),
+      'posts_per_page' => 1,
+    );
+    $last_updated = new WP_Query($args);
+
+    // get all the posts
     $postsForSitemap = get_posts(array(
         'numberposts' => -1,
         'orderby' => 'modified',
@@ -25,13 +33,34 @@ function itsg_create_sitemap() {
     foreach( $postsForSitemap as $post ) {
         setup_postdata( $post );
 
-        $postdate = explode( " ", $post->post_modified );
+	if ($post->ID == "26" or $post->ID == "237") {
+		$postdate = explode( " ", $last_updated->post->post_date );
+	} else {
+	      	$postdate = explode( " ", $post->post_modified );
+	}
+
+	$dStart = new DateTime($postdate[0]); 
+	$dEnd  = new DateTime();
+	$dDiff = $dStart->diff($dEnd);
+	$days = (int)$dDiff->format("%r%a");
+
+	if ($days < "7" or $post->ID == "2" or $post->ID == "26" or $post->ID == "237") {
+    		$freq = "daily";
+	} elseif ($days < "30") {
+    		$freq = "weekly";
+	} elseif ($days < "60") {
+    		$freq = "monthly";
+	} else {
+		$freq = "yearly";
+	}
+
 
         $sitemap .= '<url>'.
           '<loc>' . str_replace("http://www0.","https://www.",get_permalink( $post->ID )) . '</loc>' .
           '<lastmod>' . $postdate[0] . '</lastmod>' .
-          '<changefreq>monthly</changefreq>' .
+          '<changefreq>' . $freq . '</changefreq>' .
          '</url>';
+
       }
 
     $sitemap .= '</urlset>';
